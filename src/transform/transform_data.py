40e7d1bbc4568f3132178e38a93ecb47d6561ad4
Tom Davies <tom.davies@realitymine.com>
2026-04-24 00:27:36 +0100

    Add slowly-changing-dimension data transformation for player data

--- src/transform/transform_data.py
+++ src/transform/transform_data.py
@@ -1,14 +1,38 @@
 class FootballDataTransformer:
 
-    def transform_player(self, raw_data: list) -> dict:
-        """Extract a flat dict from raw player API response, suitable for DB insertion."""
-        return {
-            "id": raw_data.get("id"),
-            "first_name": raw_data.get("name", {}).get("first"),
-            "last_name": raw_data.get("name", {}).get("last"),
-            "display_name": raw_data.get("name", {}).get("display"),
-            "date_of_birth": raw_data.get("birth", {}).get("date", {}).get("millis"),
-            "country": raw_data.get("birth", {}).get("country", {}).get("country"),
-            "position": raw_data.get("info", {}).get("position"),
-            "shirt_num": raw_data.get("info", {}).get("shirtNum"),
+    def transform_player(self, raw_data: list) -> tuple[dict, list[dict]]:
+        """Transform raw player API response into a player dimension and season rows.
+
+        Returns:
+            (player_dict, list of player_season dicts)
+        """
+        first = raw_data[0]
+
+        player = {
+            "player_id": first["id"]["playerId"],
+            "first_name": first["name"]["first"],
+            "last_name": first["name"]["last"],
+            "display_name": first["name"]["display"],
+            "date_of_birth": first["dates"]["birth"],
+            "country": first["country"]["country"],
+            "country_iso": first["country"]["isoCode"],
         }
+
+        player_seasons = [
+            {
+                "player_id": entry["id"]["playerId"],
+                "season_id": entry["id"]["seasonId"],
+                "competition_id": entry["id"]["competitionId"],
+                "team_id": entry["team"]["id"],
+                "team_name": entry["team"]["name"],
+                "position": entry["position"],
+                "shirt_num": entry.get("shirtNum"),
+                "height": entry.get("height"),
+                "weight": entry.get("weight"),
+                "loan": entry["team"].get("loan", 0),
+                "joined_club": entry["dates"].get("joinedClub"),
+            }
+            for entry in raw_data
+        ]
+
+        return player, player_seasons
